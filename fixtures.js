aa05081d3cc027bb964d31f11e30c1c6239069b2
Chris Tallman <chris.tallman@beckshybrids.com>
2023-02-13 09:50:35 -0500

    Try and fix tests.

--- fixtures.js
+++ fixtures.js
@@ -1,7 +1,7 @@
 const repository = {
-  name: 'github-action-slack-notify-build',
+  name: 'github-action-slack-notify-build-status',
   owner: {
-    login: 'voxmedia',
+    login: 'tallmanbrew',
   },
 };
 
@@ -22,7 +22,7 @@ export const GITHUB_PR_EVENT = {
     payload: {
       repository,
       pull_request: {
-        html_url: 'https://github.com/voxmedia/github-action-slack-notify-build/pulls/1',
+        html_url: 'https://github.com/tallmanbrew/github-action-slack-notify-build-status/pulls/1',
         title: 'This is a PR',
         head: {
           ref: 'my-branch',
